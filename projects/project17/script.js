8ac01d147103af54257788fb0183b8cb451d82ce
SivaharanBOOM <sivabook404@outlook.com>
2026-05-01 15:44:30 +0530

    project 17 update

---
+++ projects/project17/script.js
@@ -0,0 +1,58 @@
+const main = document.getElementById('main');
+const form = document.getElementById('form');
+const search = document.getElementById('search');
+const API_URL = 'https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=8d61b07f3729cae10ea45b63f4420fab&page=1';
+const IMG_PATH = 'https://image.tmdb.org/t/p/w1280';
+const SEARCH_URL = 'https://api.themoviedb.org/3/search/movie?api_key=8d61b07f3729cae10ea45b63f4420fab&query="';
+
+async function getMovies(API_URL) {
+    const res = await fetch(API_URL);
+    const data = await res.json();
+    showMovies(data.results);
+}
+
+function showMovies(movies) {
+    main.innerHTML = '';
+    movies.forEach((movie) => {
+        const { title, poster_path, vote_average, overview } = movie;
+        const movieEl = document.createElement('div');
+        movieEl.classList.add('movie');
+        movieEl.innerHTML = `
+            <img src="${poster_path ? IMG_PATH + poster_path : ''}" alt="${title}">
+            <div class="movie-info">
+                <h3>${title}</h3>
+                <span class="${getClassByRate(vote_average)}">${vote_average}</span>
+            </div>
+            <div class="overview">
+                <h3>Overview</h3>
+                ${overview}
+            </div>
+        `;
+        main.appendChild(movieEl);
+    });
+}
+
+function getClassByRate(vote) {
+    if (vote >= 8) {
+        return 'green';
+    } else if (vote >= 5) {
+        return 'orange';
+    } else {
+        return 'red';
+    }
+}
+
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    const searchTerm = search.value;
+
+    if (searchTerm && searchTerm !== '') {
+        getMovies(SEARCH_URL + searchTerm);
+        search.value = '';
+    }
+});
+
+
+getMovies(API_URL);
